3e51b1cf0ef87ffec2707ee0750bf31eb4d7e552
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-18 16:34:16 +0200

    Création classe User

---
+++ src/app/models/user/User.ts
@@ -0,0 +1,9 @@
+export class User {
+    mail: string;
+    password: string;
+
+    constructor(mail: string, password: string) {
+        this.mail = mail;
+        this.password = password;
+    }
+}
