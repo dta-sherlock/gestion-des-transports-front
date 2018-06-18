465e0c7e24bd01002e730b49c7d0f97b913310bb
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-18 16:36:05 +0200

    Instanciation User

--- src/app/authentification/authentification.component.ts
+++ src/app/authentification/authentification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user/User';
 
 @Component({
   selector: 'app-authentification',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthentificationComponent implements OnInit {
 
+  user: User = new User("", "");
+  
   constructor() { }
 
   ngOnInit() {
