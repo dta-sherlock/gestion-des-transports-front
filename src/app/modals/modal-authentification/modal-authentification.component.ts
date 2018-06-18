aabb9abf4b1158986a6b134ec69abbd0edba2f48
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-18 18:18:34 +0200

    Revert "A revert"
    This reverts commit cf3c45c43c610b8af180df7a342d3b82ac18997e.

--- src/app/modals/modal-authentification/modal-authentification.component.ts
+++ src/app/modals/modal-authentification/modal-authentification.component.ts
@@ -1,28 +1,15 @@
-//Import core
-import { Component } from '@angular/core';
-//Import ng-bootstrap dependencies
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
-  selector: 'app-ngbd-modal-basic',
-  templateUrl: './ngbd-modal-basic.component.html'
+  selector: 'app-modal-authentification',
+  templateUrl: './modal-authentification.component.html',
+  styleUrls: ['./modal-authentification.component.css']
 })
-export class NgbdModalBasicComponent {
-  closeResult: string;
-  constructor(private modalService: NgbModal) { }
-  open(content) {
-    this.modalService.open(content).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+export class ModalAuthentificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
   }
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
+
 }
