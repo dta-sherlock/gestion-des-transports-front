ce9824f8a8a34190ef9689c7c63899b3e683f1c7
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-19 09:47:28 +0200

    a revert

--- src/app/modals/modal-authentification/modal-authentification.component.ts
+++ src/app/modals/modal-authentification/modal-authentification.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-modal-authentification',
@@ -11,6 +12,7 @@ export class ModalAuthentificationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('#myModal').modal("show")
   }
 
   openModal() {
